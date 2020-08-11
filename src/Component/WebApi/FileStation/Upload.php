cc44bd4853bf0099b7ee526aacb104f5e6e7263c
damien millet <contact@damien-millet.xyz>
2020-08-11 22:21:26 +0200

    :sparkles: Introducing new features.

--- src/Component/WebApi/FileStation/Upload.php
+++ src/Component/WebApi/FileStation/Upload.php
@@ -3,8 +3,36 @@
 
 namespace DM\SynologyBundle\Component\WebApi\FileStation;
 
-
+/**
+ * Class Upload
+ * @package DM\SynologyBundle\Component\WebApi\FileStation
+ */
 class Upload
 {
+    private const PATH = 'webapi/entry.cgi';
+    private const API = 'SYNO.FileStation.Upload';
 
+    /**
+     * @param $path
+     * @param $createParents
+     * @param $file
+     * @param $overwrite
+     * @return array
+     */
+    public function upload($path, $createParents, $file, $overwrite): array
+    {
+        return [
+            'method' => 'POST',
+            'path' => self::PATH,
+            'query' => [
+                'api' => self::API,
+                'version' => 3,
+                'method' => 'upload',
+                'path' => $path,
+                'create_parents' => $createParents,
+                'file' => $file,
+                'overwrite' => $overwrite
+            ]
+        ];
+    }
 }
